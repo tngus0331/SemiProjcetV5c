98a59ebe17cff57c7fa571ad4f75034430e922cb
tngus0331 <kim97_@naver.com>
2020-05-29 12:06:14 +0900

    보완2

--- src/main/java/suhyun/spring/mvc/service/PdsService.java
+++ src/main/java/suhyun/spring/mvc/service/PdsService.java
@@ -6,6 +6,7 @@ import suhyun.spring.mvc.dao.PdsDAO;
 import suhyun.spring.mvc.vo.PdsVO;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @Service("psrv")
 public class PdsService {
@@ -17,14 +18,11 @@ public class PdsService {
         this.pdao = pdao;
     }
 
-    public String newPds(PdsVO pd) {
+    public String newPds(PdsVO pd, Map<String, String> frmdata) {
         String result = "데이터 입력 실패!";
 
-        // 첨부파일 정보를 임의로 저장
-//        pd.setFname("abc123xyz.zip");
-//        pd.setFsize("1234");
+        procFormdata(pd, frmdata);
         pd.setFdown("0");
-//        pd.setFtype("zip");
 
         if (pdao.insertPds(pd))
             result = "데이터 입력 성공!!";
@@ -43,4 +41,20 @@ public class PdsService {
         return pdao.selectOnePds(pno);
     }
 
+    // multipart 폼 데이터 처리
+    private void procFormdata(PdsVO p, Map<String, String> frmdata) {
+
+        for(String key:frmdata.keySet()) {
+            String val = frmdata.get(key);
+            switch (key) {
+                case "title": p.setTitle(val); break;
+                case "userid": p.setUserid(val); break;
+                case "contents": p.setContents(val); break;
+
+                case "file1": p.setFname(val); break;
+                case "file1size": p.setFsize(val); break;
+                case "file1type": p.setFtype(val); break;
+            }
+        }
+    }
 }
